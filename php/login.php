9138a75e3ce2b984a75655af32aea28912f4913b
Andres A. Torres Lugo <134486510+aatNJIT@users.noreply.github.com>
2025-09-29 18:29:26 -0400

    Forgot to change paths for directories
    oops

--- php/login.php
+++ php/login.php
@@ -2,13 +2,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once('rabbitMQ/path.inc');
-require_once('rabbitMQ/get_host_info.inc');
-require_once('rabbitMQ/rabbitMQLib.inc');
-require_once('rabbitMQ/RabbitClient.php');
+require_once('../rabbitMQ/get_host_info.inc');
+require_once('../rabbitMQ/rabbitMQLib.inc');
+require_once('../rabbitMQ/RabbitClient.php');
 
 if (!isset($_POST['username']) || !isset($_POST['password'])) {
-    header('Location: index.html');
+    header('Location: ../login.html');
     exit();
 }
 
@@ -16,7 +15,7 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 if (empty($username) || empty($password) || strlen($username) > 64 || strlen($password) > 64) {
-    header('Location: index.html');
+    header('Location: ../login.html');
     exit();
 }
 
@@ -28,5 +27,5 @@ $request['password'] = $password;
 $client = RabbitClient::getConnection();
 $response = $client->send_request($request);
 
-header('Location: index.html');
+header('Location: ../login.html');
 exit();
--- rabbitMQ/RabbitClient.php
+++ rabbitMQ/RabbitClient.php
@@ -1,4 +1,5 @@
 <?php
+
 class RabbitClient
 {
     public static $connection = null;
