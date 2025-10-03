3e9125e928ae91e1853887237dc081c67fa81009
anthony <anthonyquispilaya@gmail.com>
2025-10-03 03:32:04 +0000

    connected the database vm to rabbitmq vm

---
+++ rabbitMQ/mysqlconnect.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+
+$mydb = new mysqli('192.168.56.105','it490','it490','it490');
+
+if ($mydb->errno != 0)
+{
+	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+	exit(0);
+}
+
+echo "successfully connected to users".PHP_EOL;
+
+$query = "select * from users;";
+
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+	echo "failed to execute query:".PHP_EOL;
+	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+	exit(0);
+}
+
+
+?>
+
