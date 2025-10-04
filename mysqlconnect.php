1ecb858ca4087abd3776a3666de76c8678dec49f
Sam <sm527@njit.edu>
2025-10-04 02:50:57 +0000

    Able to run RabbitMQ through own repo without need of mysqlconnect.php file

---
+++ mysqlconnect.php
@@ -0,0 +1,48 @@
+#!/usr/bin/php
+<?php
+
+mysqli_report(MYSQLI_REPORT_OFF); 
+
+$mydb = new mysqli('192.168.56.105','it490','it490','it490');
+
+if ($mydb->errno != 0)
+{
+	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+	exit(0);
+}
+
+echo "successfully connected to users".PHP_EOL;
+
+$query = "select * from users;";
+
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+	echo "failed to execute query:".PHP_EOL;
+	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+	exit(0);
+}
+// User test variables
+$test_username = "testuser";
+$test_password = "password";
+$it490db = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+$stmt = $mydb->prepare($it490db);
+
+if ($stmt !== false) {
+    $stmt->bind_param("ss", $test_username, $test_password); // PARAMETERS SENT AS STRING
+
+	if ($stmt->execute()) { // SUCCESS RESPONSE FROM IT490 DB
+		echo "Success the user $test_username was inserted." .PHP_EOL;
+	}
+	else { // FAILURE RESPONSE FROM IT490 DB
+		echo "The user $test_username failed to insert." .PHP_EOL;
+		echo "Error: ". $stmt->error .PHP_EOL;
+	}
+
+    $stmt->close();
+}
+
+$mydb->close();
+
+?>
