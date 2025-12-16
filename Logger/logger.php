f1386b062bf125efbd3711b389934434302dd72d
anthony <anthonyquispilaya@gmail.com>
2025-12-16 20:15:14 +0000

    Added logger to apache branch.

---
+++ Logger/logger.php
@@ -0,0 +1,11 @@
+<?php
+# This is a really simple decentralized logging script.
+
+function log_message($msg) {
+	$pathfile = "/var/log/it490/webserver.log";
+        $timestamp = date("m/d/Y g:i:s A");
+
+        file_put_contents($pathfile, "$timestamp - $msg\n", FILE_APPEND);
+}
+
+
