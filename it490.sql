7a58af08bfdd04340fcdc6accd8d9c96c968bb53
anthony <anthonyquispilaya@gmail.com>
2025-12-19 03:54:52 +0000

    Update: Email column added, OTP columns.

--- it490.sql
+++ it490.sql
@@ -1,243 +1,159 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1deb3
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.44, for Linux (x86_64)
 --
--- Host: localhost:3306
--- Generation Time: Dec 16, 2025 at 04:28 AM
--- Server version: 8.0.43-0ubuntu0.24.04.2
--- PHP Version: 8.3.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: it490
+-- ------------------------------------------------------
+-- Server version	8.0.44-0ubuntu0.24.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `it490`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Catalogs`
---
-
-CREATE TABLE `Catalogs` (
-  `CatalogID` int NOT NULL,
-  `UserID` int NOT NULL,
-  `Title` varchar(255) NOT NULL,
-  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `Catalog_Comments`
 --
 
+DROP TABLE IF EXISTS `Catalog_Comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Catalog_Comments` (
-  `CommentID` int NOT NULL,
+  `CommentID` int NOT NULL AUTO_INCREMENT,
   `CatalogID` int NOT NULL,
   `UserID` int NOT NULL,
   `Text` text NOT NULL,
-  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`CommentID`),
+  KEY `CommentCatalogID` (`CatalogID`),
+  KEY `CommentUserID` (`UserID`),
+  CONSTRAINT `CommentCatalogID` FOREIGN KEY (`CatalogID`) REFERENCES `Catalogs` (`CatalogID`) ON DELETE CASCADE,
+  CONSTRAINT `CommentUserID` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Catalog_Games`
 --
 
+DROP TABLE IF EXISTS `Catalog_Games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Catalog_Games` (
   `CatalogID` int NOT NULL,
   `AppID` int NOT NULL,
   `Rating` int NOT NULL,
-  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`CatalogID`,`AppID`),
+  KEY `CatalogAppID` (`AppID`),
+  CONSTRAINT `CatalogAppID` FOREIGN KEY (`AppID`) REFERENCES `Games` (`AppID`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `CatalogID` FOREIGN KEY (`CatalogID`) REFERENCES `Catalogs` (`CatalogID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `Catalogs`
+--
+
+DROP TABLE IF EXISTS `Catalogs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Catalogs` (
+  `CatalogID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`CatalogID`),
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `UserID` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Followers`
 --
 
+DROP TABLE IF EXISTS `Followers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Followers` (
-  `FollowerID` int NOT NULL,
+  `FollowerID` int NOT NULL AUTO_INCREMENT,
   `FollowedID` int NOT NULL,
-  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`FollowerID`),
+  KEY `FollowedID` (`FollowedID`),
+  CONSTRAINT `FollowedID` FOREIGN KEY (`FollowedID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `FollowerID` FOREIGN KEY (`FollowerID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Games`
 --
 
+DROP TABLE IF EXISTS `Games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Games` (
   `AppID` int NOT NULL,
   `Name` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Tags` json DEFAULT NULL,
-  `Description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
+  `Description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`AppID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Sessions`
 --
 
+DROP TABLE IF EXISTS `Sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Sessions` (
-  `SessionID` bigint NOT NULL,
+  `SessionID` bigint NOT NULL AUTO_INCREMENT,
   `UserID` int NOT NULL,
   `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+  `Activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`SessionID`),
+  KEY `userID` (`UserID`),
+  CONSTRAINT `SessionUserID` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8460065075645404042 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Users`
 --
 
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Users` (
-  `ID` int NOT NULL,
-  `SteamID` bigint UNSIGNED DEFAULT NULL,
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `SteamID` bigint unsigned DEFAULT NULL,
   `Username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Email` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) NOT NULL,
+  `OTP` varchar(6) DEFAULT NULL,
+  `OTP_Expiry` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `SteamID` (`SteamID`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Catalogs`
---
-ALTER TABLE `Catalogs`
-  ADD PRIMARY KEY (`CatalogID`),
-  ADD KEY `UserID` (`UserID`);
-
---
--- Indexes for table `Catalog_Comments`
---
-ALTER TABLE `Catalog_Comments`
-  ADD PRIMARY KEY (`CommentID`),
-  ADD KEY `CommentCatalogID` (`CatalogID`),
-  ADD KEY `CommentUserID` (`UserID`);
-
---
--- Indexes for table `Catalog_Games`
---
-ALTER TABLE `Catalog_Games`
-  ADD PRIMARY KEY (`CatalogID`,`AppID`),
-  ADD KEY `CatalogAppID` (`AppID`);
-
---
--- Indexes for table `Followers`
---
-ALTER TABLE `Followers`
-  ADD PRIMARY KEY (`FollowerID`),
-  ADD KEY `FollowedID` (`FollowedID`);
-
---
--- Indexes for table `Games`
---
-ALTER TABLE `Games`
-  ADD PRIMARY KEY (`AppID`);
-
---
--- Indexes for table `Sessions`
---
-ALTER TABLE `Sessions`
-  ADD PRIMARY KEY (`SessionID`),
-  ADD KEY `userID` (`UserID`);
-
---
--- Indexes for table `Users`
---
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `SteamID` (`SteamID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Catalogs`
---
-ALTER TABLE `Catalogs`
-  MODIFY `CatalogID` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Catalog_Comments`
---
-ALTER TABLE `Catalog_Comments`
-  MODIFY `CommentID` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Followers`
---
-ALTER TABLE `Followers`
-  MODIFY `FollowerID` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Sessions`
---
-ALTER TABLE `Sessions`
-  MODIFY `SessionID` bigint NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Users`
---
-ALTER TABLE `Users`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Catalogs`
---
-ALTER TABLE `Catalogs`
-  ADD CONSTRAINT `UserID` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `Catalog_Comments`
---
-ALTER TABLE `Catalog_Comments`
-  ADD CONSTRAINT `CommentCatalogID` FOREIGN KEY (`CatalogID`) REFERENCES `Catalogs` (`CatalogID`) ON DELETE CASCADE,
-  ADD CONSTRAINT `CommentUserID` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE;
-
---
--- Constraints for table `Catalog_Games`
---
-ALTER TABLE `Catalog_Games`
-  ADD CONSTRAINT `CatalogAppID` FOREIGN KEY (`AppID`) REFERENCES `Games` (`AppID`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  ADD CONSTRAINT `CatalogID` FOREIGN KEY (`CatalogID`) REFERENCES `Catalogs` (`CatalogID`) ON DELETE CASCADE;
-
---
--- Constraints for table `Followers`
---
-ALTER TABLE `Followers`
-  ADD CONSTRAINT `FollowedID` FOREIGN KEY (`FollowedID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FollowerID` FOREIGN KEY (`FollowerID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE;
-
---
--- Constraints for table `Sessions`
---
-ALTER TABLE `Sessions`
-  ADD CONSTRAINT `SessionUserID` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-19  3:40:15
